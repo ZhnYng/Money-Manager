2129e9a3156b835cda3739bd84e26f4e483b2183
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2023-03-25 23:53:16 +0800

    changes

--- frontend/src/components/BottomNavbar.jsx
+++ frontend/src/components/BottomNavbar.jsx
@@ -4,7 +4,7 @@ import { Link, NavLink } from 'react-router-dom';
 
 export default function BottomNavbar(){
   return (
-    <div className="btm-nav btm-nav-lg bg-white rounded-t-3xl">
+    <div className="btm-nav btm-nav-sm bg-white rounded-t-3xl">
       <NavLink 
         to="/" 
         className={({ isActive }) =>
@@ -12,7 +12,7 @@ export default function BottomNavbar(){
         }
       >
         <button className="text-inherit">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
         </button>
       </NavLink>
       <NavLink 
@@ -22,7 +22,7 @@ export default function BottomNavbar(){
         }
       >
         <button className="text-green-500">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
         </button>
       </NavLink>
       <NavLink 
@@ -32,7 +32,7 @@ export default function BottomNavbar(){
         }
       >
         <button className="text-green-500">
-          <FaUserCircle size={35} />
+          <FaUserCircle size={30} />
         </button>
       </NavLink>
     </div>
--- frontend/src/components/TransferData.jsx
+++ frontend/src/components/TransferData.jsx
@@ -75,7 +75,7 @@ export default function TransferData({details, setChangesMade}){
     {/* Start of transaction modal */}
     <input type="checkbox" id={`transaction-modal-${details.transaction_id}`} className="modal-toggle" />
     <label htmlFor={`transaction-modal-${details.transaction_id}`} className="modal cursor-pointer">
-      <div className={`card w-11/12 ${categoryColors[category]?.bgColor} text-white my-2`}>
+      <div className={`card w-10/12 ${categoryColors[category]?.bgColor} text-white my-2`}>
         <div className="card-body">
           <div className='flex items-center justify-between'>
             <p className='text-2xl underline font-bold'>{details.recipient}</p>
@@ -106,14 +106,14 @@ export default function TransferData({details, setChangesMade}){
     </label>
     {/* End of transaction modal */}
     <label htmlFor={`transaction-modal-${details.transaction_id}`}>
-      <div className={`card w-96 ${categoryColors[category]?.bgColor} text-white my-2`}>
+      <div className={`card w-80 ${categoryColors[category]?.bgColor} text-white my-2`}>
         <div className={`badge bg-white ${categoryColors[category]?.textColor} font-bold border-none self-end mt-2 mr-2`}>{category?.toUpperCase()}</div>
         <div className="card-body flex-row justify-between pt-0 grid-cols-12 grid">
           <div className='col-span-6'>
             <p className='text-xl font-bold'>{details.recipient}</p>
             <h2 className="card-title text-xl">{convertDate(details.date_of_transfer)}</h2>
           </div>
-          <h2 className="card-title text-2xl font-bold col-span-6 text-center justify-self-center">{details.transaction_type === "income" ? "+" : "-"}{details.amount}</h2>
+          <h2 className="card-title text-xl font-bold col-span-6 text-center justify-self-center">{details.transaction_type === "income" ? "+" : "-"}{details.amount}</h2>
         </div>
       </div>
     </label>
