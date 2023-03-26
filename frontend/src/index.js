3172d9562a621138a6371759ae2f809a38cdda7a
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2023-03-26 10:54:24 +0800

    oops

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://money-manager-backend-no0w.onrender.com';
+axios.defaults.baseURL = 'https://money-manager-backend-no0w.onrender.com';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
