1c46b1afefd4e7e6d92c75d6deba445e6b7b20c7
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2023-03-28 13:57:05 +0800

    trying to fix login issue

--- backend/control/app.js
+++ backend/control/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ["https://moneymanagerclient.netlify.app"]
+        origin: ["http://localhost:3000", "https://moneymanagerclient.netlify.app"]
     })
 )
 app.use(express.json())
