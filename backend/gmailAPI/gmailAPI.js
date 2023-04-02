f78b8ebf9010d6101ebbb20f4efebdbb89c083f4
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2023-04-02 17:34:18 +0800

    trying again

--- backend/gmailAPI/gmailAPI.js
+++ backend/gmailAPI/gmailAPI.js
@@ -10,7 +10,7 @@ let oauth2Client = new google.auth.OAuth2(
   "430806173435-041j4g6133jfj4noqg676ppr6pkpdjg0.apps.googleusercontent.com",
   "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",
   // "http://localhost:3000" //frontend url
-  "https://moneymanagerclient.netlify.app/" //frontend url
+  "https://moneymanagerclient.netlify.app/login" //frontend url
 )
 
 // // If modifying these scopes, delete token.json.
