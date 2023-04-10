467ab6816368347fa387315e247d92474c25199b
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2023-04-10 16:35:34 +0800

    shoudl work

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,6 +1,8 @@
 const pgp = require('pg-promise')(/* options */)
+
+const db = pgp(process.env.LOCAL_RENDER);
 // const db = pgp('postgres://money_manager_admin:money_manager@localhost:5432/money_manager') //Local
-const db = pgp('postgres://money_manager_admin:c3PaaP9l8cnnGH8mdxP5gbf55YAeaVtX@dpg-cgf9miseoogqfc44q2d0-a/money_manager_x0zs') //locally On Render
+// const db = pgp('postgres://money_manager_admin:c3PaaP9l8cnnGH8mdxP5gbf55YAeaVtX@dpg-cgf9miseoogqfc44q2d0-a/money_manager_x0zs') //locally On Render
 // const db = pgp('postgres://money_manager_admin:c3PaaP9l8cnnGH8mdxP5gbf55YAeaVtX@dpg-cgf9miseoogqfc44q2d0-a.singapore-postgres.render.com/money_manager_x0zs?ssl=true') //externally from Render
 // Append the following to the above to prevent the error "SSL/TLS required" ?ssl=true
 
