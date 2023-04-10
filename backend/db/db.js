a1a8535f98e1d74409a0993ebbeb08f105824eb1
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2023-04-10 22:44:18 +0800

    updated badges

--- backend/db/db.js
+++ backend/db/db.js
@@ -5,7 +5,6 @@ var local_render_db_key = process.env.LOCAL_RENDER;
 var local_db_key = process.env.LOCAL_KEY;
 var neon_db_key = process.env.NEON_DB_KEY;
 
-// const db = pgp(local_render_db_key);
 const db = pgp(neon_db_key);
 
 module.exports = db;
