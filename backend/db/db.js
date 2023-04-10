f89d5f81976bc1cf0efe27151f28c3d6db553d9f
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2023-04-10 13:47:12 +0800

    fixed env

--- backend/db/db.js
+++ backend/db/db.js
@@ -6,16 +6,7 @@ var er = process.env.EXTERNAL_RENDER;
 var lr = process.env.LOCAL_RENDER
 // const db = pgp('postgres://money_manager_admin:money_manager@localhost:5432/money_manager') //Local
 //const db = pgp('postgres://money_manager_admin:c3PaaP9l8cnnGH8mdxP5gbf55YAeaVtX@dpg-cgf9miseoogqfc44q2d0-a/money_manager_x0zs') //locally On Render
-const db = pgp(lr) //externally from Render
+const db = pgp(lk) //externally from Render
 // Append the following to the above to prevent the error "SSL/TLS required" ?ssl=true
 
 module.exports = db;
-
-// To edit postgresql DB
-// Server [localhost]: dpg-cgf9miseoogqfc44q2d0-a.singapore-postgres.render.com
-// Database [postgres]: money_manager_x0zs
-// Port [5432]: 
-// Username [postgres]: money_manager_admin
-// Password for user money_manager_admin: c3PaaP9l8cnnGH8mdxP5gbf55YAeaVtX
-
-// postgres://USER:PASSWORD@EXTERNAL_HOST:PORT/DATABASE
