6ce42bb130e64325813d52fd8445e18ecb3676c6
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2023-04-24 00:13:47 +0800

    pls wor

--- backend/gmailAPI/objectify.js
+++ backend/gmailAPI/objectify.js
@@ -173,7 +173,7 @@ const objectify = {
           let time = new Date(`1970-01-01 ${timeDetails[0]}`);
           console.log(`time: ${time}`)
           console.log(`timeDetails[0]: ${timeDetails[0]}`)
-          let formattedTime = time.toLocaleTimeString("en-UK", { hour12: false });
+          let formattedTime = time.toLocaleTimeString("en-Gb", { hour12: false });
           outputObject["Time_of_Transfer"] = formattedTime;
           console.log(outputObject["Time_of_Transfer"]) // Error value here: 24:05:00
           break;
