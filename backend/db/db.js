a7bf766d448d16606bc5ff6302d2408289213eee
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2023-06-05 23:04:33 +0800

    initial commit

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,8 +4,8 @@ require('dotenv').config()
 var local_render_db_key = process.env.LOCAL_RENDER;
 var local_db_key = process.env.LOCAL_KEY;
 var neon_db_key = process.env.NEON_DB_KEY;
-var NEON_DB_DBS_BRANCH_KEY = process.env.NEON_DB_DBS_BRANCH_KEY;
+var db_staging_key = process.env.DB_STAGING_KEY;
 
-const db = pgp(neon_db_key);
+const db = pgp(db_staging_key);
 
 module.exports = db;
