fce9f28f14b8bd196f42ef0e4c12fec7ff0d9029
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2023-06-19 23:12:24 +0800

    omg forgot to change axios defaults

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://money-manager-backend-no0w.onrender.com';
-axios.defaults.baseURL = 'https://money-manager-backend-test.onrender.com';
+axios.defaults.baseURL = 'https://money-manager-backend-no0w.onrender.com';
+// axios.defaults.baseURL = 'https://money-manager-backend-test.onrender.com';
 // axios.defaults.baseURL = 'http://localhost:5000';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
